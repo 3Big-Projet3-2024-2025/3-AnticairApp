b6d9fdb595f5a9a866232ccb6120822f508aa3ef
Tsuyoshi <akitsuyoshi11@gmail.com>
2024-11-20 12:06:05 +0100

    Push Arborescence

---
+++ AnticairApi/src/main/java/be/anticair/anticairapi/AnticairApiApplication.java
@@ -0,0 +1,13 @@
+package be.anticair.anticairapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnticairApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnticairApiApplication.class, args);
+    }
+
+}
