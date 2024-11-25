3f80547b71e08ea1287c5325f7eed66988917399
LA219263 <101389918+ZarzyckiAlexis@users.noreply.github.com>
2024-11-25 22:22:47 +0100

    Adding Imports & Providers

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './auth.service';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Function to initialize Keycloak
+export function initializeKeycloak(authService: AuthService) {
+  return (): Promise<any> => {
+    return authService.initKeycloak(); // Call the init method from AuthService
+  };
+}
 
 @NgModule({
   declarations: [
@@ -10,9 +21,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      deps: [AuthService], // Ensure AuthService is injected
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
