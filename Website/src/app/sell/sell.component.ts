608413206e7bf80f9b57e46b386ea858de5a22c2
LA219263 <101389918+ZarzyckiAlexis@users.noreply.github.com>
2024-11-25 22:42:37 +0100

    Adding Sell UI
    Adding the UI of the page on the component

--- Website/src/app/sell/sell.component.ts
+++ Website/src/app/sell/sell.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-sell',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './sell.component.css'
 })
 export class SellComponent {
+
+  currentTheme: 'dark' | 'light' = 'light'; // Actual theme, by default light
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    // Subscribe to Theme event
+    this.themeService.theme$.subscribe(theme => {
+      this.currentTheme = theme;
+    });
+
+  }
 
 }
