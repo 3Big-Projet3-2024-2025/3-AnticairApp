29442d2923873cf644e0233eeeff77bb1a2b5baa
LA219263 <101389918+ZarzyckiAlexis@users.noreply.github.com>
2024-11-25 22:37:32 +0100

    Adding UI
    Adding the footer UI

--- Website/src/app/footer/footer.component.ts
+++ Website/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  currentTheme: 'dark' | 'light' = 'light'; // Actual theme, by default light
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    // Subscribe to Theme event
+    this.themeService.theme$.subscribe(theme => {
+      this.currentTheme = theme;
+    });
+
+  }
 
 }
