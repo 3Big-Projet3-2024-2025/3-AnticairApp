94d943085f1dcb2293c7256644ab20dfe0f54ed1
la227716 <la227716@student.helha.be>
2024-11-30 23:23:32 +0100

    create ListingService and ListingController for antique sales management
    Created ListingService and ListingController to implement the core functionality for managing antique sales listings, including CRUD operations and business logic.

---
+++ Api/src/main/java/be/anticair/anticairapi/keycloak/controller/ListingController.java
@@ -0,0 +1,9 @@
+package be.anticair.anticairapi.keycloak.controller;
+
+
+/**
+ * REST Controller for managing listing in the database.
+ * @Author Blommaert Youry
+ */
+public class ListingController {
+}
