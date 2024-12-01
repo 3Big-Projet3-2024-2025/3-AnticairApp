ef9b06c6ccceb7ee976d5301dcd75e880e39d20b
LA219263 <101389918+ZarzyckiAlexis@users.noreply.github.com>
2024-12-02 00:01:27 +0100

    Add user management component with theme support and responsive design
    #NEED TO BE FIX: Responsivity (color bug)

--- Website/src/app/admin/users/users.component.ts
+++ Website/src/app/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemeService } from '../../theme.service';
 
 @Component({
   selector: 'app-users',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+
+  currentTheme: 'dark' | 'light' = 'light'; // Actual theme, by default light
+
+  constructor(private themeService : ThemeService) { }
+
+
+  ngOnInit(){
+    // Subscribe to Theme event
+    this.themeService.theme$.subscribe(theme => {
+      this.currentTheme = theme;
+    });
+  }
 }
