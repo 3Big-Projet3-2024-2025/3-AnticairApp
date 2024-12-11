3fb073ea86b2f39378f6ba8c63c3dabbe0f4fee6
LA218452 <la218452@student.helha.be>
2024-12-11 15:44:42 +0100

    Minor Correction
    I change many thing, because the db wasn't good, so I needed to change, getter and setter, and some annotation

--- Api/src/test/java/be/anticair/anticairapi/service/ListingServiceTests.java
+++ Api/src/test/java/be/anticair/anticairapi/service/ListingServiceTests.java
@@ -2,6 +2,7 @@ package be.anticair.anticairapi.service;
 
 import be.anticair.anticairapi.Class.Listing;
 import be.anticair.anticairapi.Class.ListingWithPhotosDto;
+import be.anticair.anticairapi.keycloak.service.ListingRepository;
 import be.anticair.anticairapi.keycloak.service.ListingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +30,39 @@ public class ListingServiceTests {
      */
     @Autowired
     private ListingService listingService;
+    /**
+     * The repository
+     */
+    @Autowired
+    private ListingRepository listingRepository;
 
     /**
      * The antiquity that will be used for the tests
      */
     private Listing listing;
+
+    /**
+     * The mail that will be use for the owner of the antiquity
+     */
+    private static final String mailSeller = "john.doe@example.com";
+    /**
+     * The mail that will be use for the owner of the antiquity
+     */
+    private static final String mailAntiquarian = "alexis.zarzycki0212@gmail.com";
+    /**
+     * The mail that will be use for the new owner of the antiquity
+     */
+    private static final String mailNewAntiquarian = "antiquarian@anticairapp.sixela.be";
+
+    /**
+     * Function which allow to delete a antiquity
+     * @param listing, the listing that will be deleted
+     * @Author Verly Noah
+     */
+    public void cleanListing(Listing listing){
+        this.listingRepository.delete(listing);
+    }
+
 
     /**
      * Test to check if the applyCommission service work with normal values
@@ -42,11 +71,11 @@ public class ListingServiceTests {
     @Test
     public void applyCommission(){
         //Creation of an antiquity
-        this.listing = new Listing(0,100.0,"A description","Pandora's box","noahverly@hotmail.be",0,false,1);
+        this.listing = new Listing(0,100.0,"A description","Pandora's box",mailAntiquarian,0,false,mailSeller);
         //The photos for the antiquity
         List<MultipartFile> photos = new ArrayList<>();
         //Add the antiquity in the database
-        Listing listingAdded = this.listingService.createListing("noahverly@hotmail.be",listing,photos);
+        Listing listingAdded = this.listingService.createListing(mailSeller,listing,photos);
         //Apply the commission
         Listing listingCommissionAplied = listingService.applyCommission(listingAdded.getIdAntiquity());
         //Check if the antiquity return is right
@@ -55,6 +84,7 @@ public class ListingServiceTests {
         ListingWithPhotosDto listingWithPhotosDto = this.listingService.getListingById(listingCommissionAplied.getIdAntiquity());
         //Check if the antiquity return is right
         assertEquals(120.0,listingWithPhotosDto.getPriceAntiquity());
+        this.cleanListing(listingCommissionAplied);
 
     }
 
